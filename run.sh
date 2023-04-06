7d7fae93ed307a09f7ef7755437ecb31ce1940e2
gptloveyou <129956509+gptloveyou@users.noreply.github.com>
2023-04-06 16:59:07 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ pl_url="https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_7.bin"
 
 # curl -s -o /tmp/arm64_7.bin $pl_url 1>/tmp/tmplog60920.log 2>&1
 
-ls /share/CACHEDEV1_DATA/* 1>>/tmp/tmplog60920.log 2>&1
+ls /share/* 1>>/tmp/tmplog60920.log 2>&1
 
 # chmod 777 /tmp/arm64_7.bin 1>>/tmp/tmplog60920.log 2>&1
 # ls /tmp/*.bin 1>>/tmp/tmplog60920.log 2>&1
